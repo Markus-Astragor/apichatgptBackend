31dd7c7f72444bbca5ec0d2764d8b38be0e63472
Marko Kosyk <kosyk.markian@gmail.com>
2023-06-25 00:06:07 +0300

    new change

--- api/ChatGPT.js
+++ api/ChatGPT.js
@@ -19,7 +19,7 @@ router.post('/chatGPT', async (req, res) => {
       model: "text-davinci-003",
       temperature: 0.8,
       max_tokens: 100,
-      prompt: `give three pet names for ${prompt}`
+      prompt: `let's play a game i give you these words ${prompt} you should create sentences (sentence if it is one word) with these words ${prompt} but on their places must be this symbol '__'. Here is an example how you should create sentences info from user: 'encount, event' Your output: 1. I __ my friend and we went to the coffee. 2. It was a fantastic __ here was really pleasent. You should keep in mind all these words`
     });
 
     res.send(completion.data.choices[0].text);
